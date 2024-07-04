808a02116e4ca9c1ac8de6c7049d5b650ed125df
Audyari Wiyono <audy123ari@gmail.com>
2024-07-04 15:49:57 +0700

    SortedSetApp

---
+++ src/SortedSet/SortedSetApp.java
@@ -0,0 +1,29 @@
+package SortedSet;
+
+import DATA.Person;
+import DATA.PersonComparator;
+
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SortedSetApp {
+
+    public static void main(String[] args) {
+
+        SortedSet<Person> people = new TreeSet<>(new PersonComparator());
+
+        people.add(new Person("Eko"));
+        people.add(new Person("Budi"));
+        people.add(new Person("Joko"));
+
+        for (var person : people){
+            System.out.println(person.getName());
+        }
+
+        SortedSet<Person> sortedSet = Collections.unmodifiableSortedSet(people);
+        // sortedSet.add(new Person("Kurniawan"));
+
+    }
+}
+
