2a85ceb8090b332480bcc6f4b4c77b864a6a57f1
Audyari Wiyono <audy123ari@gmail.com>
2024-07-05 15:03:21 +0700

    LegacyCollection

---
+++ src/LegacyCollection/StackApp.java
@@ -0,0 +1,20 @@
+package LegacyCollection;
+
+import java.util.Stack;
+
+public class StackApp {
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Eko");
+        stack.push("Kurniawan");
+        stack.push("Khannedy");
+
+        for(var value = stack.pop(); value != null; value = stack.pop()){
+            System.out.println(value);
+        }
+
+    }
+}
